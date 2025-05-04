57c5040a5e414b577ea8a91042acf4bd0530632a
Muhammed Salah <96310606+mosala7201484@users.noreply.github.com>
2025-05-04 07:52:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Numerics;
+
+namespace CarpetCleaning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Enjoy with our offers for this month \n");
+            //int priceOfSmallCarpt = 15;
+            ///int priceOfbigCarpet = 25;
+            Console.WriteLine("Enter Your number of small Carpet \n");
+            int SmallNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Your number of Big Carpet\n");
+            int BigNum = Convert.ToInt32(Console.ReadLine());
+            int SmallCarpet = SmallNum * 25;
+            int BigCarpet = BigNum * 35;
+            int Sum = SmallCarpet + BigCarpet;
+            double Tax = Sum * .06;
+            double Total = Tax + Sum;
+            Console.WriteLine(" Your bill is " + Sum + " and the Tax is 6% the Total is\n ");
+            Console.WriteLine("========================");
+            Console.WriteLine(Total);
+            Console.WriteLine("This estimate is valid for 30 days");
+        }
+    }
+
+}
+
+
